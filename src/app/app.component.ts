564012afc09ce02acebb7f80ad52f3c2ea06c36d
Anton Moiseev <a@antonmoiseev.com>
2017-07-11 19:06:03 +0400

    Add home module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
-  `,
-  styles: []
+    <h1>App component</h1>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+
+export function homeModuleLoader() {
+  return HomeModule;
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', loadChildren: homeModuleLoader }
+    ],
+    {
+      enableTracing: true
+    })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
